7f25648a2bd9e87ebe2a1ea7c77570fbd8b6ae75
SurekhaRangam <surekharangam51@gmail.com>
2025-02-23 09:50:28 +0530

    coming to intial state

--- notesapp/src/components/NotesCard.jsx
+++ notesapp/src/components/NotesCard.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { useNotes } from './context/NotesContext'
 import { findArchivenotes } from './utils/findArchivenoes'
-import { findImportantnotes } from './utils/findImportantnotes'
-import {findDeletednotes} from './utils/findDeletednotes'
 const NotesCard = ({id,title,text,isPinned}) => {
-    const {notesdispatch,archive,importantnotes,deletednotes}=useNotes()
+    const {notesdispatch,archive}=useNotes()
     const Pinned=(id)=>{
         !isPinned ?
         notesdispatch({
@@ -19,8 +17,6 @@ const NotesCard = ({id,title,text,isPinned}) => {
     
 
     const isArchivenotes=findArchivenotes(archive,id)
-    const isImportantnotes=findImportantnotes(importantnotes,id)
-    const isdeletednotes=findDeletednotes(deletednotes,id)
     
     const addArchive=(id)=>{
       !isArchivenotes ?
@@ -33,23 +29,6 @@ const NotesCard = ({id,title,text,isPinned}) => {
         payload:{id}
       })
     }
-    const addImportant=(id)=>{
-      notesdispatch({
-        type:'add_to_important',
-        payload:{id}
-      })
-    }
-    const onDelete=(id)=>{
-      !isdeletednotes ? 
-      notesdispatch({
-        type:'add_to_bin',
-        payload:{id}
-      }) :
-      notesdispatch({
-        type:'remove_from_bin',
-        payload:{id}
-      })
-    }
 
     
     
@@ -58,26 +37,18 @@ const NotesCard = ({id,title,text,isPinned}) => {
         <div className='w-[300px] border-2 border-gray-300 p-3 rounded-md shadow-md mt-5'>
       <div className='flex items-center relative'>
                 <p>{title}</p>
-                {(!isArchivenotes && !isdeletednotes) ?
+                  {!isArchivenotes &&
                 <button onClick={()=>Pinned(id)}className='absolute top-0 right-0 '>
                 <span className={isPinned ? 'material-icons' : 'material-icons-outlined'}>push_pin</span>
-                </button>: <></>}
+                </button>}
                 
                 </div>
                 <hr />
                 <div className='flex items-center'>
-                  <div className='flex flex-col'>
                   <p>{text}</p>
-                  <div>
-                    {!isdeletednotes &&
-                  <button onClick={()=>addImportant(id)}><span className='material-icons-outlined "absolute bottom-0 left-0 '>label</span></button>}
-                  </div>
-                  </div>
-                  
                   <div className="ml-auto mt-2">
-                    {!isdeletednotes &&
-                  <button onClick={()=>addArchive(id)}><span className={isArchivenotes ? 'material-icons':'material-icons-outlined'}>archive</span></button>}
-                  <button onClick={()=>onDelete(id)}>{!isdeletednotes ? <span className='material-icons-outlined'>delete</span> : <span className='material-icons'>restore_from_trash</span>}</button>
+                  <button onClick={()=>addArchive(id)}><span className={isArchivenotes ? 'material-icons':'material-icons-outlined'}>archive</span></button>
+                  <button><span className='material-icons-outlined'>delete</span></button>
                 </div>
                 </div>
     </div>
